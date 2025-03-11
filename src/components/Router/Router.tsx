6c7ee1d3ef86ddc1f26624c8f0a5d2c18cf67fc0
Juliana Negreiros <ju_bsnegreiros@hotmail.com>
2025-03-11 04:27:00 +0000

    Add new about page

--- src/components/Router/Router.tsx
+++ src/components/Router/Router.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Page from '../../pages/v1/Home/components/Page';
+import About from '../../pages/v2/About';
 
 const Router: React.FC = () => (
   <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<About />} />
+    </Routes>
     <Routes>
       <Route path='/v1/' element={<Page />} />
     </Routes>
